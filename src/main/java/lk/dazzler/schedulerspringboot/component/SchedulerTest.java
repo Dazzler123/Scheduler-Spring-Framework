181a4fc4bb6b9baa52931cbd153f2b7cb39772d9
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-13 13:01:55 +0530

    Content updated

--- src/main/java/lk/dazzler/schedulerspringboot/component/SchedulerTest.java
+++ src/main/java/lk/dazzler/schedulerspringboot/component/SchedulerTest.java
@@ -6,5 +6,20 @@
  */
 package lk.dazzler.schedulerspringboot.component;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SchedulerTest {
+    int i = 0;
+
+    @Scheduled(fixedRate = 1000)  //call runTest() every 1 second looped
+    public void runTest(){
+        System.out.println("Display output count : " + i);
+        i++;
+        if(i > 5){
+            System.out.println("=========== Output greater than 5 ==========");
+            i = i - 5;
+        }
+    }
 }

cc2052cd319d888ee7008adb251636db1b74d01b
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-13 12:53:35 +0530

    content updatd

--- src/main/java/lk/dazzler/schedulerspringboot/SchedulerConfig.java
+++ src/main/java/lk/dazzler/schedulerspringboot/SchedulerConfig.java
@@ -6,5 +6,10 @@
  */
 package lk.dazzler.schedulerspringboot;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
 public class SchedulerConfig {
 }

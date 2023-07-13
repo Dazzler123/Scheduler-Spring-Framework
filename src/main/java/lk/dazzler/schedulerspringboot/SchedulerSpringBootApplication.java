fc927d6087c45be175fe1e0d32ac5e25fc68dd2a
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-13 12:50:56 +0530

    Initial Commit

---
+++ src/main/java/lk/dazzler/schedulerspringboot/SchedulerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package lk.dazzler.schedulerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/dazzler/schedulerspringboot/SchedulerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dazzler.schedulerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

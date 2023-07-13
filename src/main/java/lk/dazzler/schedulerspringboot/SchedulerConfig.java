2996c861b2ada015396087fb403402e7a5325cbc
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-13 12:53:01 +0530

    Introduced SchedulerConfig

---
+++ src/main/java/lk/dazzler/schedulerspringboot/SchedulerConfig.java
@@ -0,0 +1,10 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 13-Jul-2023
+ * Time : 12:52 PM
+ * Project Name : Scheduler-Spring-Boot
+ */
+package lk.dazzler.schedulerspringboot;
+
+public class SchedulerConfig {
+}
